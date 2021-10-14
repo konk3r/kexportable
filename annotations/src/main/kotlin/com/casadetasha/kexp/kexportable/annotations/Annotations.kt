9b1477ab39d05edb510f9595d56db4b32af30b60
Gabriel Spencer <konk3r@gmail.com>
2021-10-14 15:35:30 -0500

    Do not include functions when getting classes

--- annotations/src/main/kotlin/com/casadetasha/kexp/kexportable/annotations/Annotations.kt
+++ annotations/src/main/kotlin/com/casadetasha/kexp/kexportable/annotations/Annotations.kt
@@ -3,7 +3,7 @@ package com.casadetasha.kexp.kexportable.annotations
 import com.casadetasha.kexp.kexportable.annotations.Kexportable.NamingConvention.AS_WRITTEN
 
 @Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.CLASS)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 annotation class Kexportable(val exportName: String = "", val namingConvention: NamingConvention = AS_WRITTEN) {
     enum class NamingConvention {
         AS_WRITTEN,
