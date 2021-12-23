1ec824210c8d6aff38c3f1d4d83ff60b5231ae90
Gabriel Spencer <konk3r@gmail.com>
2021-12-23 15:27:58 -0800

    Move annotation processing setup into AnnotationParser

--- processor/src/main/kotlin/com/casadetasha/kexp/kexportable/processor/KexportableProcessor.kt
+++ processor/src/main/kotlin/com/casadetasha/kexp/kexportable/processor/KexportableProcessor.kt
@@ -1,6 +1,9 @@
 package com.casadetasha.kexp.kexportable.processor
 
 import com.casadetasha.kexp.annotationparser.AnnotationParser
+import com.casadetasha.kexp.annotationparser.AnnotationParser.KAPT_KOTLIN_GENERATED_OPTION_NAME
+import com.casadetasha.kexp.annotationparser.AnnotationParser.getClassesAnnotatedWith
+import com.casadetasha.kexp.annotationparser.AnnotationParser.kaptKotlinGeneratedDir
 import com.casadetasha.kexp.annotationparser.kxt.getClassesAnnotatedWith
 import com.casadetasha.kexp.kexportable.annotations.Kexportable
 import com.google.auto.service.AutoService
@@ -10,31 +13,28 @@ import javax.lang.model.SourceVersion
 import javax.lang.model.element.TypeElement
 
 @AutoService(Processor::class)
-@SupportedOptions(KexportableProcessor.KAPT_KOTLIN_GENERATED_OPTION_NAME)
+@SupportedOptions(KAPT_KOTLIN_GENERATED_OPTION_NAME)
 @SupportedSourceVersion(SourceVersion.RELEASE_11)
 @OptIn(KotlinPoetMetadataPreview::class)
 class KexportableProcessor : AbstractProcessor() {
-    private lateinit var kaptKotlinGeneratedDir: String
-
-    companion object {
-        const val KAPT_KOTLIN_GENERATED_OPTION_NAME = "kapt.kotlin.generated"
-    }
 
     override fun getSupportedAnnotationTypes() = mutableSetOf(
         Kexportable::class.java.canonicalName
     )
 
     override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment?): Boolean {
-        kaptKotlinGeneratedDir = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME] ?: return false
-        if (roundEnv == null) return false
-        AnnotationParser.setup(processingEnv)
+        try {
+            AnnotationParser.setup(processingEnv, roundEnv)
+        } catch (_: IllegalStateException) {
+            return false
+        }
 
-        generateClasses(roundEnv)
+        generateClasses()
         return true
     }
 
-    private fun generateClasses(roundEnv: RoundEnvironment) {
-        val kexportedClasses = roundEnv.getClassesAnnotatedWith(Kexportable::class)
+    private fun generateClasses() {
+        val kexportedClasses = getClassesAnnotatedWith(Kexportable::class)
         kexportedClasses.forEach { kotlinClass ->
             val kexportableAnnotation = kotlinClass.getAnnotation(Kexportable::class)!!
             val kexportableClass = KexportableClass(kotlinClass, kexportableAnnotation as Kexportable)
