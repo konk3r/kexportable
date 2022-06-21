172bc1f5541b3456a98c640a301e97c2c1e85751
Gabriel Spencer <gabrielspencer@gmail.com>
2022-06-20 19:54:19 -0700

    copy files to prepare for kexportable split

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "path-to-plunder"
+include(
+    "kexp:petals:annotations",
+    "kexp:petals:post-processing-tests",
+    "kexp:petals:processor",
+    "kexp:kexportable:annotations",
+    "kexp:kexportable:processor",
+    "kexp:kexportable:post-processing-tests")
